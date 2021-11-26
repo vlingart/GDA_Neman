738e6ac0efa324b46ec9e0f8086bbc51dd1a6ba5
HasagiGr <91342573+HasagiGr@users.noreply.github.com>
2021-11-26 12:52:42 +0300

    def for estv

--- estv.py
+++ estv.py
@@ -1,1 +1,20 @@
-
+def estv(words):
+    f = open('bigramm.txt', 'r')  
+    dataset = {}
+    for line in f:
+        for i in range(676):
+            s = line.split('\t')
+            dataset[s[0]] = float(s[1][:-1])*10
+    let = open('letters.txt', 'r')  
+    letter_dataset = {}
+    for lines in let:
+        for i in range(26):
+            q = lines.split(',')
+            letter_dataset[q[0]] = float(q[1][:-1])
+    estesvennost = []
+    for word in words:
+        estestvennost_slova = 1
+        for i in range(len(word)-1):
+            estestvennost_slova *= dataset[word[i+1]+word[i]]
+        estesvennost.append(estestvennost_slova)
+    return(estesvennost)
