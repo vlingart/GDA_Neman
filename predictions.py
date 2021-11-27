580ca262f7d1c577bc3f99c1569765e6ebbe4e8c
HasagiGr <91342573+HasagiGr@users.noreply.github.com>
2021-11-27 12:09:10 +0300

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -1,41 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-def get_train_data(Data_train):
-    Data_Chisl_train=[]
-    Data_dlin_train=[]
-    Data_est_train=[]
-    for i in range(len(Data_train[0])):
-        Data_Chisl_train.append(Data_train[0][i][0:36])
-        Data_dlin_train.append(Data_train[0][i][37])
-        Data_est_train.append(Data_train[0][i][36])
-    Data_y=Data_train[1]
-    Data_Chisl_train=np.array(Data_Chisl_train)
-    Data_dlin_train=np.array(Data_dlin_train).reshape(-1,1)
-    Data_est_train=np.array(Data_est_train).reshape(-1,1)
-    return(Data_Chisl_train,Data_dlin_train,Data_est_train)
-
-
-# In[ ]:
-
-
-def get_valid_data(Data_valid):
-    Data_Chisl_valid=[]
-    Data_dlin_valid=[]
-    Data_est_valid=[]
-    for i in range(len(Data_valid[0])):
-        Data_Chisl_valid.append(Data_valid[0][i][0:36])
-        Data_dlin_valid.append(Data_valid[0][i][37])
-        Data_est_valid.append(Data_valid[0][i][36])
-    Data_y_valid=Data_valid[1]
-    Data_Chisl_valid=np.array(Data_Chisl_valid)
-    Data_dlin_valid=np.array(Data_dlin_valid).reshape(-1,1)
-    Data_est_valid=np.array(Data_est_valid).reshape(-1,1)
-    return(Data_Chisl_valid,Data_dlin_valid,Data_est_valid)
-
 
 # In[ ]:
 
