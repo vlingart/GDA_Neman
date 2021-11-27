0f1743a9f7552347c14a553d19a87340a0505552
vlingart <95068839+vlingart@users.noreply.github.com>
2021-11-27 17:20:16 +0300

    Add files via upload

---
+++ DNG_REC.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+from predictions import get_predictions
+from predictions import data_prep_pred
+from Data_prep import Data_Prep
+from sklearn.neural_network import MLPClassifier
+import numpy as np
+from turn_into_vector import words_into_vector
+from get_data import get_data
+from estv import estv
+t=MLPClassifier((20)*100, random_state=1,activation='logistic',max_iter=3000,warm_start=True)
+y=np.array([0]).reshape(1,-1)
+x=np.array([0]*36).reshape(1,-1)
+t.fit(x,y)
+with open('weights.txt','r') as r:
+    for i in range(len(t.coefs_)):
+        for j in range(len(t.coefs_[i])):
+            for l in range(len(t.coefs_[i][j])): 
+                t.coefs_[i][j][l]=r.readline()
+print('Введите путь к файлу(csv) или домен')
+inp=input()
+if(inp[-4:]=='.csv'):
+    recogn_file()
+else:
+    recogn_domen()
+
